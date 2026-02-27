c82696628643969f1493566b1f67c8734299e84e
Kenzo Van Craeynest <kenzo.vancraeynest@vlaanderen.be>
2026-02-27 14:30:09 +0100

    feat(DECIBEL-1706): add ProjectController + GlobalExceptionHandler

---
+++ app/src/main/java/be/vlaanderen/omgeving/bezwaarschriften/config/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package be.vlaanderen.omgeving.bezwaarschriften.config;
+
+import be.vlaanderen.omgeving.bezwaarschriften.project.ProjectNietGevondenException;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * Gecentraliseerde exception handling voor REST controllers.
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  /**
+   * Handelt ProjectNietGevondenException af met HTTP 404.
+   *
+   * @param e De exception
+   * @return Gestructureerde foutrespons
+   */
+  @ExceptionHandler(ProjectNietGevondenException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public Map<String, List<Map<String, Object>>> handleProjectNietGevonden(
+      ProjectNietGevondenException e) {
+    return Map.of("messages", List.of(
+        Map.of("code", "project.not-found",
+            "parameters", Map.of("naam", e.getProjectNaam()))
+    ));
+  }
+}
