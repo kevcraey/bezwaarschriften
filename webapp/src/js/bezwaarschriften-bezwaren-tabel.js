2a28810f4c5a8cadc8ee25eb12200f7e6bb1c6a1
Kenzo Van Craeynest <kenzo.vancraeynest@vlaanderen.be>
2026-02-27 14:31:51 +0100

    feat(DECIBEL-1706): add bezwaarschriften-bezwaren-tabel component

---
+++ webapp/src/js/bezwaarschriften-bezwaren-tabel.js
@@ -0,0 +1,77 @@
+import {BaseHTMLElement, defineWebComponent, registerWebComponents} from '@domg-wc/common';
+import {VlTableComponent} from '@domg-wc/components/block/table/vl-table.component.js';
+import {vlGlobalStyles} from '@domg-wc/styles';
+
+registerWebComponents([VlTableComponent]);
+
+const STATUS_LABELS = {
+  'todo': 'Te verwerken',
+  'extractie-klaar': 'Extractie klaar',
+  'fout': 'Fout',
+  'niet ondersteund': 'Niet ondersteund',
+};
+
+export class BezwaarschriftenBezwarenTabel extends BaseHTMLElement {
+  static get properties() {
+    return {
+      bezwaren: {type: Array},
+    };
+  }
+
+  constructor() {
+    super(`
+      <style>${vlGlobalStyles}</style>
+      <vl-table>
+        <table>
+          <thead>
+            <tr>
+              <th>Bestandsnaam</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody id="tabel-body"></tbody>
+        </table>
+      </vl-table>
+    `);
+    this.__bezwaren = [];
+  }
+
+  set bezwaren(waarde) {
+    this.__bezwaren = waarde || [];
+    this._renderRijen();
+  }
+
+  get bezwaren() {
+    return this.__bezwaren;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._renderRijen();
+  }
+
+  _renderRijen() {
+    const tbody = this.shadowRoot && this.shadowRoot.querySelector('#tabel-body');
+    if (!tbody) return;
+
+    if (this.__bezwaren.length === 0) {
+      tbody.innerHTML = '<tr><td colspan="2">Geen bestanden gevonden</td></tr>';
+      return;
+    }
+
+    tbody.innerHTML = this.__bezwaren
+        .map((b) => `<tr>
+          <td>${this._escapeHtml(b.bestandsnaam)}</td>
+          <td>${STATUS_LABELS[b.status] || b.status}</td>
+        </tr>`)
+        .join('');
+  }
+
+  _escapeHtml(str) {
+    const div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+}
+
+defineWebComponent(BezwaarschriftenBezwarenTabel, 'bezwaarschriften-bezwaren-tabel');
